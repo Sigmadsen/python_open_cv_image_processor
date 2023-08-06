6132e19d2e639bee503ff6708a6344a1d616b418
Sigmandsen <you@example.com>
2023-08-06 13:41:02 +0300

    add grayscale converting

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def make_grayscale():
+    image = cv2.imread('data/input/1.jpg', cv2.IMREAD_GRAYSCALE)
+    # cv2.imshow("Image", image)
+    # cv2.waitKey(0)
+    cv2.imwrite('data/output/1.jpg', image)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    make_grayscale()
